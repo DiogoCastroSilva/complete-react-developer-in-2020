b9efea10edab1d1b6b9ba2aa5b38fb3cd82d7f12
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 13:45:03 +0100

    Clean monsters

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,20 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: '1'
-        },
-        {
-          name: 'Dracula',
-          id: '2'
-        },
-        {
-          name: 'Zombie',
-          id: '3'
-        }
-      ]
+      monsters: []
     };
   }
 
