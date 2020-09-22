7e5640d111b16a4b758a343c5fff559eb5fec89a
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 17:00:53 +0100

    Finishing styling th app, by adding custom font and more color

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Mosnters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value})}
--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.styles.css';
 export const Card = ({monster}) => (
     <div className='card-container'>
     <img alt='monster' src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-        <h1>{monster.name}</h1>
+        <h2>{monster.name}</h2>
         <p>{monster.email}</p>
     </div>
 );
