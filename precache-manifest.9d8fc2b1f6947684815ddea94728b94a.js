dfdb36723e557b6d87a2981f011177276972a5fe
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 17:16:59 +0100

    Updates

---
+++ precache-manifest.9d8fc2b1f6947684815ddea94728b94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f7394899c4cd9824f95162ea24fe24",
+    "url": "/complete-react-developer-in-2020/index.html"
+  },
+  {
+    "revision": "aeb85c38b5576acee057",
+    "url": "/complete-react-developer-in-2020/static/css/main.830113ed.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/complete-react-developer-in-2020/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/complete-react-developer-in-2020/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb85c38b5576acee057",
+    "url": "/complete-react-developer-in-2020/static/js/main.42c5b6ad.chunk.js"
+  },
+  {
+    "revision": "83cef6b6c15349da08ae",
+    "url": "/complete-react-developer-in-2020/static/js/runtime-main.0190ba29.js"
+  }
+]);
