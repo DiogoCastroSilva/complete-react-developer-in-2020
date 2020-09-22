f096c8a4b202826e2ede9ca9f19b6fa4a9527970
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 14:15:29 +0100

    Create a Search Box component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -25,10 +26,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monsters'
-          onChange={e => this.setState({ searchField: e.target.value})}
+          handleChange={e => this.setState({ searchField: e.target.value})}
         />
         <CardList monsters={filteredMonsters}/>
       </div>
