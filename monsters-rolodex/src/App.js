f7205bb675a62ed4c09c4e83bd69cd0d9c7a00b2
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 11:26:08 +0100

    Get the monsters fom api

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -23,6 +23,12 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data => data.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
