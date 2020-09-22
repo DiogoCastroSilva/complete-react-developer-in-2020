41cf3fc0c72f0587b84244415696c74171a3c088
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-22 11:21:20 +0100

    Start the monsters rolodex project

---
+++ monsters-rolodex/README.md
@@ -0,0 +1,3 @@
+# Monsters Rolodex
+
+Creating a basic app using React. This app show a list of monsters and let's the user search for a specific one using a input.
---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id: '3'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
