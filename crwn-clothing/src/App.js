427d8a1d9d3e1d4ea35562713b5a2e5a31bb105e
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-09-24 16:16:01 +0100

    Adding Header component and links

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,10 +5,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home/home.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/shop' component={Shop} />
