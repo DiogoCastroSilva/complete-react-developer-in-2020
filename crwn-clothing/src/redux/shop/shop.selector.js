1c8b16d7867950d96d11bdf8442a594aad7b37ef
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-10-07 11:02:39 +0100

    Normalize data

--- crwn-clothing/src/redux/shop/shop.selector.js
+++ crwn-clothing/src/redux/shop/shop.selector.js
@@ -1,14 +1,6 @@
 import { createSelector } from "reselect";
 
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -18,7 +10,5 @@ export const selectShopCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectShopCollections],
-    (collections) => collections.find(
-        colection => colection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    (collections) => collections[collectionUrlParam]
 );
