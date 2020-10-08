e7dece98230a7d9eab65ec629f970f0a30405996
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-10-08 18:00:56 +0100

    Update CollectionItem to styled components

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 import CustomButton from '../custom-button/custom-button.component';
 
--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import './collection-item.styles.scss';
+import {
+    CollectionItemContainer,
+    BackgroundImageContainer,
+    CollectionFooterContainer,
+    NameContainer,
+    PriceContainer,
+    AddToCartButton
+} from './collection-item.styles';
 
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
     return (
-        <div className='collection-item'>
-            <div
-                className='image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton
-                className='custom-button'
+        <CollectionItemContainer>
+            <BackgroundImageContainer imageUrl={imageUrl} />
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddToCartButton
                 color='inverted'
                 onClick={() => addItem(item)}
             >
                 Add To Cart
-            </CustomButton>
-        </div>
+            </AddToCartButton>
+        </CollectionItemContainer>
     );
 }
 
