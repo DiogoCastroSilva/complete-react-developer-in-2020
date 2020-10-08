af32669c67873220954b93cc0e78cd1e6fc60496
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-10-08 16:42:23 +0100

    Update CartDropdown component to styled component

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-import './cart-dropdown.styles.scss';
+import {
+    CartDropdownContainer, CartItemsContainer,
+    EmptyMessageContainer, CartDropdownButtonContainer
+} from './cart-dropdown.styles';
 
-import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
+    <CartDropdownContainer className='cart-dropdown'>
+        <CartItemsContainer>
             {cartItems.length ?
                 cartItems.map(item => (
                     <CartItem
@@ -20,18 +22,18 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
                         item={item}
                     />
                 )) :
-                <span className='empty-message'>Your cart is empty</span>
+                <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
             }
-        </div>
-        <CustomButton
+        </CartItemsContainer>
+        <CartDropdownButtonContainer
             onClick={() => {
                 history.push('/checkout');
                 dispatch(toggleCartHidden());
             }}
         >
             GO TO CHECKOUT
-        </CustomButton>
-    </div>
+        </CartDropdownButtonContainer>
+    </CartDropdownContainer>
 );
 
 const mapStateToProps = state => {
