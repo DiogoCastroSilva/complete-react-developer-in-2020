4cae7f6bc61c7d942fe1a4a00708e89b016812e4
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-11-16 16:53:19 +0000

    Change front end Strpe code

--- crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price}) => {
     const priceForStripe = price * 100;
-    const publishableKey = 'pk_live_emn24VPW77CMaoYbMNskwSON005bhLVmV2';
+    const publishableKey = 'pk_test_51GsQlAAxJTVl3mJaCeYOLbetzpqvxgi8BlqBZfo5PmnAudCnVPXNsI8AQ9AGg8fMKylVKKoqGS8mF16GwKS6ootJ00JvpHFMQi';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment successfull');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.error(`Payment error: ${JSON.stringify(error)}`);
+            alert('There was an inssue with your payment. Please make sure you use the provided credit card.');
+        });
     };
 
     return (
