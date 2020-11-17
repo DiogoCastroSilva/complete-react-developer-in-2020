43700d4a36321cea8af94ba6a6d6924113a943fc
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-11-17 08:49:56 +0000

    First way to use Context API in the Collection page component

--- crwn-clothing-context-api/src/pages/collection/collection.component.jsx
+++ crwn-clothing-context-api/src/pages/collection/collection.component.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionsContext from '../../context/collections/collections.context';
+
+
+const CollectionPage = ({ match }) => {
+
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+      {
+        collections => {
+          const collection = collections[match.params.collectionId];
+          const { title, items } = collection;
+          return (
+            <div className='collection-page'>
+              <h2 className='title'>{title}</h2>
+              <div className='items'>
+                {items.map(item => (
+                  <CollectionItem key={item.id} item={item} />
+                ))}
+              </div>
+          </div>
+          );
+        }
+      }
+    </CollectionsContext.Consumer>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
