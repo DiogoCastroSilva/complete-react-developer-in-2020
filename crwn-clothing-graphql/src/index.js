73ff76cc83c21c333606925f9b6fbd7249f4edfe
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-11-19 14:15:35 +0000

    Create project to use Graph QL with Apollo

---
+++ crwn-clothing-graphql/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// Graph QL
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+
+import { store, persistor } from './redux/store';
+
+import './index.css';
+import App from './App';
+
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client.query({
+  query: gql`
+    {
+      getCollectionsByTitle(title: "hats") {
+        id,
+        items {
+          id,
+          name,
+          price,
+          imageUrl
+        }
+      }
+    }
+  `
+}).then(response => console.log(response));
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
