f3cea3eacea191bfd8a9fe032752be40f8e68276
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-11-22 17:27:21 +0000

    Improve performance for Cart Item component adding memo

--- crwn-clothing/client/src/components/cart-item/cart-item.component.jsx
+++ crwn-clothing/client/src/components/cart-item/cart-item.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import {
     CartItemContainer,
@@ -22,4 +22,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     </CartItemContainer>
 );
 
-export default CartItem;
+export default memo(CartItem);
