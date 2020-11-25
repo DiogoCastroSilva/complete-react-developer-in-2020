d58525091734883973ab7c89ec21c44071d5e1da
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-11-25 14:25:31 +0000

    Testing with mocks and spies

--- intro-tests/async.js
+++ intro-tests/async.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 
 const getPeople = async (fetchAPI = fetch) => {
-    const getRequest = await fetchAPI('https://swapi.py4e.com/api/people/');
+    const getRequest = await fetchAPI('https://swapi.py4e.com/api/people');
     const data = await getRequest.json();
 
     return {
--- intro-tests/async.test.js
+++ intro-tests/async.test.js
@@ -1,12 +1,32 @@
 const getPeople = require('./async');
 
 
-describe('Async testes for getting people API', () => {
+describe('Async tests for getting people API', () => {
     it('calls api to get people', async () => {
         expect.assertions(2);
 
         const data = await getPeople();
-        
+
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+    });
+
+    it('getPeople returns count and results', async () => {
+        const mockFetch = jest
+            .fn()
+            .mockReturnValue(Promise.resolve({
+                json: () => Promise.resolve({
+                    count: 87,
+                    results: [0, 1, 2, 3, 4, 5]
+                })
+            }));
+
+        expect.assertions(4);
+
+        const data = await getPeople(mockFetch);
+
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people');
         expect(data.count).toEqual(87);
         expect(data.results.length).toBeGreaterThan(5);
     });
